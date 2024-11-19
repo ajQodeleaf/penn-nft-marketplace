67b0193fe8a34acad267dc404214c53fba5cbf4f
Aradhya Jain <ajain@qodeleaf.com>
2024-11-19 12:08:31 +0530

    Config: Hardhat configuration for deploying and verify contract

---
+++ backend/hardhat.config.js
@@ -0,0 +1,25 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { INFURA_SEPOLIA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: INFURA_SEPOLIA_RPC_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY || "",
+  },
+};
