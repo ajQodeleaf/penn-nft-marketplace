ca53534407ad1e6b9c3adbd1d35b5459b4ced83e
Aradhya Jain <ajain@qodeleaf.com>
2024-11-19 12:11:27 +0530

    Feat: Get the NFTMarketplace contract instance using Contract address

---
+++ backend/utils/contractInstance.js
@@ -0,0 +1,24 @@
+const { ethers } = require("hardhat");
+require("dotenv").config();
+const contractABI =
+  require("../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json").abi;
+
+const {
+  INFURA_SEPOLIA_API_KEY,
+  PRIVATE_KEY,
+  NFT_MARKETPLACE_CONTRACT_ADDRESS,
+} = process.env;
+
+const provider = new ethers.InfuraProvider("sepolia", INFURA_SEPOLIA_API_KEY);
+
+const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+
+const nftMarketplaceAddress = NFT_MARKETPLACE_CONTRACT_ADDRESS;
+
+const nftMarketplace = new ethers.Contract(
+  nftMarketplaceAddress,
+  contractABI,
+  wallet
+);
+
+module.exports = nftMarketplace;
