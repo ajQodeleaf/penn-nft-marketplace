26221b6b9be2fcaa470ffde39e71b2dd8e795a7b
Aradhya Jain <ajain@qodeleaf.com>
2024-11-22 16:10:09 +0530

    Routes: Routes for users, transactions, listings, nfts and earnings

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,20 +1,56 @@
 const express = require("express");
+const router = express.Router();
 const {
+  getUser,
+  createUser,
+  createCollection,
+  createNFT,
+  getCollectionByContract,
+  verifyNFT,
+  createTransaction,
+  getNFTById,
+  updateNFT,
+  deleteNFT,
+  getTransactionHistoryByNFTId,
+  getAllListings,
+  getListing,
+  getEarnings,
+  getAllCollections,
+  getAllNFTs,
+  getUserByWallet,
+  updateUser,
+  getAllTransactions,
+  getAllUsers,
   listNFT,
   buyNFT,
-  getEarnings,
-  getAllListings,
-  getAllUsers,
-  getListing
 } = require("../controllers/controllers");
 
-const router = express.Router();
-
-router.post("/list", listNFT);
-router.post("/buy/:listingId", buyNFT);
-router.get("/earnings", getEarnings);
-router.get("/listings", getAllListings);
-router.get("/list/:listingId", getListing);
 router.get("/users", getAllUsers);
+router.get("/user/:id", getUser);
+router.get("/user/:walletAddress", getUserByWallet);
+router.post("/user", createUser);
+router.put("/user/:id", updateUser);
+
+router.get("/collections", getAllCollections);
+router.get("/collection/:contractAddress", getCollectionByContract);
+router.post("/collection", createCollection);
+
+router.get("/nfts", getAllNFTs);
+router.get("/nft/:id", getNFTById);
+router.post("/nft", createNFT);
+router.put("/nft/:id", updateNFT);
+router.delete("/nft/:id", deleteNFT);
+router.post("/nft/list", listNFT);
+router.post("/nft/buy/:listingId", buyNFT);
+router.post("/nft/verify/:id", verifyNFT);
+
+router.get("/transactions", getAllTransactions);
+router.get("/transaction/:nftId", getTransactionHistoryByNFTId);
+router.post("/transaction", createTransaction);
+
+router.get("/listings", getAllListings);
+router.get("/listing/:listingId", getListing);
+
+router.get("/earnings", getEarnings);
 
 module.exports = router;
