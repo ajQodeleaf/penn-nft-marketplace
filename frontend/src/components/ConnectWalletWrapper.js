503e2bf2100804a05c1e299e884c491ddb4133ea
Aradhya Jain <ajain@qodeleaf.com>
2024-11-27 10:12:27 +0530

    Refactor: Connect Wallet Wrapper to show first onboarding and then connect

--- frontend/src/components/ConnectWalletWrapper.js
+++ frontend/src/components/ConnectWalletWrapper.js
@@ -1,16 +1,32 @@
 "use client";
+import { useState } from "react";
 import { useWallet } from "../context/WalletContext";
 import BottomNavigationBar from "./BottomNavigationBar";
 import ConnectWallet from "./ConnectWallet";
+import Onboarding from "./Onboarding";
 
 export default function ConnectWalletWrapper({ children }) {
   const { isConnected } = useWallet();
-  return isConnected ? (
+  const [onboardingComplete, setOnboardingComplete] = useState(false);
+
+  const completeOnboarding = () => {
+    setOnboardingComplete(true);
+  };
+
+  return (
     <>
-      {children}
-      <BottomNavigationBar />
+      {!onboardingComplete ? (
+        <Onboarding onComplete={completeOnboarding} />
+      ) : isConnected ? (
+        <>
+          {children}
+          <BottomNavigationBar />
+        </>
+      ) : (
+        <>
+          <ConnectWallet />
+        </>
+      )}
     </>
-  ) : (
-    <ConnectWallet />
   );
 }
