b64c2d5b1eb87ded4b2f65eede423f2fe8cec07f
Aradhya Jain <ajain@qodeleaf.com>
2024-11-27 12:22:28 +0530

    Update: NFT Marketplace contract

--- backend/contracts/NFTMarketplace.sol
+++ backend/contracts/NFTMarketplace.sol
@@ -17,8 +17,11 @@ contract NFTMarketplace {
     struct Collection {
         address owner;
         string name;
+        string description;
         string metadataURI;
+        uint256 price;
         uint256 createdAt;
+        bool isVerified;
     }
 
     struct CollectionStats {
@@ -39,6 +42,7 @@ contract NFTMarketplace {
 
     address private admin;
     uint256 private constant MIN_LISTING_PRICE = 0.000001 ether;
+    uint256 private constant MIN_COLLECTION_LISTING_PRICE = 0.00001 ether;
 
     event NFTListed(
         uint256 indexed listingId,
@@ -46,7 +50,8 @@ contract NFTMarketplace {
         address indexed nftContract,
         uint256 tokenId,
         uint256 price,
-        string metadataURI
+        string metadataURI,
+        string description
     );
     event NFTBought(
         uint256 indexed listingId,
@@ -59,16 +64,42 @@ contract NFTMarketplace {
         uint256 indexed collectionId,
         address indexed owner,
         string name,
-        string metadataURI
+        string description,
+        string metadataURI,
+        uint256 price
     );
     event CollectionUpdated(
         uint256 indexed collectionId,
         string name,
-        string metadataURI
+        string description,
+        string metadataURI,
+        uint256 price
     );
     event CollectionDeleted(uint256 indexed collectionId);
     event EarningsWithdrawn(address indexed seller, uint256 amount);
     event NFTVerified(uint256 indexed listingId, bool isVerified);
+    event NFTCollectionListed(
+        uint256 indexed collectionId,
+        address indexed owner,
+        address nftContract
+    );
+    event NFTCollectionCreated(
+        uint256 indexed collectionId,
+        address indexed owner,
+        string name,
+        string description,
+        string metadataURI,
+        uint256 price
+    );
+    event NFTCollectionUpdated(
+        uint256 indexed collectionId,
+        string name,
+        string description,
+        string metadataURI,
+        uint256 price
+    );
+    event NFTCollectionDeleted(uint256 indexed collectionId);
+    event NFTCollectionVerified(uint256 indexed collectionId, bool isVerified);
 
     modifier isOwner(
         address nftContract,
@@ -98,58 +129,32 @@ contract NFTMarketplace {
 
     function createCollection(
         string calldata name,
-        string calldata metadataURI
+        string calldata description,
+        string calldata metadataURI,
+        uint256 price
     ) external onlyAdmin {
+        require(price >= MIN_COLLECTION_LISTING_PRICE, "Price too low");
+
         collections[collectionCounter] = Collection(
             msg.sender,
             name,
+            description,
             metadataURI,
-            block.timestamp
+            price,
+            block.timestamp,
+            false
         );
 
         emit CollectionCreated(
             collectionCounter,
             msg.sender,
             name,
-            metadataURI
+            description,
+            metadataURI,
+            price
         );
 
         collectionCounter++;
-    }
-
-    function getCollection(
-        uint256 collectionId
-    ) external view returns (Collection memory) {
-        require(
-            bytes(collections[collectionId].name).length > 0,
-            "Collection does not exist"
-        );
-        return collections[collectionId];
-    }
-
-    function updateCollection(
-        uint256 collectionId,
-        string calldata name,
-        string calldata metadataURI
-    ) external onlyAdmin {
-        Collection storage collection = collections[collectionId];
-        require(bytes(collection.name).length > 0, "Collection does not exist");
-
-        collection.name = name;
-        collection.metadataURI = metadataURI;
-
-        emit CollectionUpdated(collectionId, name, metadataURI);
-    }
-
-    function deleteCollection(uint256 collectionId) external onlyAdmin {
-        require(
-            bytes(collections[collectionId].name).length > 0,
-            "Collection does not exist"
-        );
-
-        delete collections[collectionId];
-
-        emit CollectionDeleted(collectionId);
     }
 
     function listNFT(
@@ -187,51 +192,21 @@ contract NFTMarketplace {
             nftContract,
             tokenId,
             price,
-            metadataURI
+            metadataURI,
+            description
         );
 
         listingCounter++;
     }
 
-    function buyNFT(uint256 listingId) external payable isListed(listingId) {
-        Listing memory listing = listings[listingId];
+    function getCollection(
+        uint256 collectionId
+    ) external view returns (Collection memory) {
         require(
-            msg.value >= listing.price,
-            "Insufficient funds to buy this NFT"
+            bytes(collections[collectionId].name).length > 0,
+            "Collection does not exist"
         );
-
-        IERC721(listing.nftContract).transferFrom(
-            address(this),
-            msg.sender,
-            listing.tokenId
-        );
-
-        updateTradingVolume(listing.nftContract, listing.price);
-
-        sellerEarnings[listing.seller] += msg.value;
-
-        userPurchases[msg.sender].push(listingId);
-
-        emit NFTBought(
-            listingId,
-            msg.sender,
-            listing.nftContract,
-            listing.tokenId,
-            listing.price
-        );
-
-        delete listings[listingId];
-    }
-
-    function getCollectionCount() external view returns (uint256) {
-        return collectionCounter;
-    }
-
-    function updateTradingVolume(address nftContract, uint256 price) private {
-        CollectionStats storage stats = collectionStats[nftContract];
-        stats.previousVolume = stats.tradingVolume;
-        stats.tradingVolume += price;
-        stats.lastUpdated = block.timestamp;
+        return collections[collectionId];
     }
 
     function getListing(
@@ -250,12 +225,96 @@ contract NFTMarketplace {
         return userPurchases[user];
     }
 
-    function getAdmin() external view returns (address) {
-        return admin;
+    function updateCollection(
+        uint256 collectionId,
+        string calldata name,
+        string calldata description,
+        string calldata metadataURI,
+        uint256 price
+    ) external onlyAdmin {
+        Collection storage collection = collections[collectionId];
+        require(bytes(collection.name).length > 0, "Collection does not exist");
+        require(price >= MIN_COLLECTION_LISTING_PRICE, "Price too low");
+
+        collection.name = name;
+        collection.description = description;
+        collection.metadataURI = metadataURI;
+        collection.price = price;
+
+        emit CollectionUpdated(
+            collectionId,
+            name,
+            description,
+            metadataURI,
+            price
+        );
     }
 
-    function getListingCount() external view returns (uint256) {
-        return listingCounter;
+    function updateNFTListing(
+        uint256 listingId,
+        uint256 price,
+        string calldata metadataURI,
+        string calldata description
+    ) external isListed(listingId) {
+        Listing storage listing = listings[listingId];
+        require(
+            listing.seller == msg.sender,
+            "Only the seller can update the listing"
+        );
+        require(
+            price >= MIN_LISTING_PRICE,
+            "Price must be at least 0.000001 ETH"
+        );
+
+        listing.price = price;
+        listing.metadataURI = metadataURI;
+        listing.description = description;
+
+        emit NFTListed(
+            listingId,
+            msg.sender,
+            listing.nftContract,
+            listing.tokenId,
+            price,
+            metadataURI,
+            description
+        );
+    }
+
+    function deleteCollection(uint256 collectionId) external onlyAdmin {
+        require(
+            bytes(collections[collectionId].name).length > 0,
+            "Collection does not exist"
+        );
+        delete collections[collectionId];
+        emit CollectionDeleted(collectionId);
+    }
+
+    function buyNFT(uint256 listingId) external payable isListed(listingId) {
+        Listing memory listing = listings[listingId];
+        require(
+            msg.value >= listing.price,
+            "Insufficient funds to buy this NFT"
+        );
+
+        IERC721(listing.nftContract).transferFrom(
+            address(this),
+            msg.sender,
+            listing.tokenId
+        );
+        updateTradingVolume(listing.nftContract, listing.price);
+
+        sellerEarnings[listing.seller] += msg.value;
+        userPurchases[msg.sender].push(listingId);
+
+        emit NFTBought(
+            listingId,
+            msg.sender,
+            listing.nftContract,
+            listing.tokenId,
+            listing.price
+        );
+        delete listings[listingId];
     }
 
     function withdrawEarnings() external {
@@ -276,5 +335,47 @@ contract NFTMarketplace {
         listing.isVerified = true;
 
         emit NFTVerified(listingId, true);
+    }
+
+    function verifyCollection(uint256 collectionId) external onlyAdmin {
+        Collection storage collection = collections[collectionId];
+        require(bytes(collection.name).length > 0, "Collection does not exist");
+        require(!collection.isVerified, "Collection is already verified");
+
+        collection.isVerified = true;
+
+        emit NFTCollectionVerified(collectionId, true);
+    }
+
+    function getAdmin() external view returns (address) {
+        return admin;
+    }
+
+    function updateTradingVolume(address nftContract, uint256 price) private {
+        CollectionStats storage stats = collectionStats[nftContract];
+        stats.previousVolume = stats.tradingVolume;
+        stats.tradingVolume += price;
+        stats.lastUpdated = block.timestamp;
+    }
+
+    function getCollectionStats(
+        address nftContract
+    ) external view returns (CollectionStats memory) {
+        return collectionStats[nftContract];
+    }
+
+    function getRankings() external view returns (address[] memory) {
+        uint256 length = collectionCounter;
+        address[] memory rankedCollections = new address[](length);
+        uint256 index = 0;
+
+        for (uint256 i = 0; i < length; i++) {
+            if (collections[i].isVerified) {
+                rankedCollections[index] = collections[i].owner;
+                index++;
+            }
+        }
+
+        return rankedCollections;
     }
 }
