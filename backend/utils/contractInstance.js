0ff638347d6e20fe6611a7f2f228191b1e68f55f
Aradhya Jain <ajain@qodeleaf.com>
2024-12-10 22:15:38 +0530

    Refactor: Import statements and make sure environment variables are present on hand

--- backend/utils/contractInstance.js
+++ backend/utils/contractInstance.js
@@ -1,7 +1,8 @@
-const { ethers } = require("hardhat");
-require("dotenv").config();
-const nftMarketplaceContractABI =
-  require("../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json").abi;
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+import nftMarketplaceArtifact from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json" assert { type: "json" };
+
+dotenv.config();
 
 const {
   INFURA_SEPOLIA_API_KEY,
@@ -9,16 +10,24 @@ const {
   NFT_MARKETPLACE_CONTRACT_ADDRESS,
 } = process.env;
 
+if (
+  !INFURA_SEPOLIA_API_KEY ||
+  !PRIVATE_KEY ||
+  !NFT_MARKETPLACE_CONTRACT_ADDRESS
+) {
+  throw new Error(
+    "Missing one or more required environment variables: INFURA_SEPOLIA_API_KEY, PRIVATE_KEY, NFT_MARKETPLACE_CONTRACT_ADDRESS"
+  );
+}
+
 const provider = new ethers.InfuraProvider("sepolia", INFURA_SEPOLIA_API_KEY);
 
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
-const nftMarketplaceAddress = NFT_MARKETPLACE_CONTRACT_ADDRESS;
-
 const nftMarketplace = new ethers.Contract(
-  nftMarketplaceAddress,
-  nftMarketplaceContractABI,
+  NFT_MARKETPLACE_CONTRACT_ADDRESS,
+  nftMarketplaceArtifact.abi,
   wallet
 );
 
-module.exports = { nftMarketplace, wallet };
+export { nftMarketplace, wallet };
