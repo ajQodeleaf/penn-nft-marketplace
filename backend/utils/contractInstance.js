908b2a8e5b5584330c17a0bfb1b31bddda375eef
Aradhya Jain <ajain@qodeleaf.com>
2024-12-11 00:15:16 +0530

    Refactor: In ES modules, there is no built-in __dirname variable, but using new URL(import.meta.url).pathname allows to derive the current file's path

--- backend/utils/contractInstance.js
+++ backend/utils/contractInstance.js
@@ -1,8 +1,22 @@
 import { ethers } from "ethers";
 import dotenv from "dotenv";
-import nftMarketplaceArtifact from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+let resolvedPath = path.resolve(
+  __dirname,
+  "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json"
+);
+
+resolvedPath = resolvedPath.slice(3);
+
+const nftMarketplaceArtifact = JSON.parse(
+  fs.readFileSync(resolvedPath, "utf8")
+);
 
 const {
   INFURA_SEPOLIA_API_KEY,
@@ -21,7 +35,6 @@ if (
 }
 
 const provider = new ethers.InfuraProvider("sepolia", INFURA_SEPOLIA_API_KEY);
-
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
 const nftMarketplace = new ethers.Contract(
