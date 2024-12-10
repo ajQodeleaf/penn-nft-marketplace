0e6d358662f961df036966cbddbb9a91409c574f
Aradhya Jain <ajain@qodeleaf.com>
2024-12-10 19:40:32 +0530

    Refactor: Log and execute specific functionality based on NODE_ENV (development/production).

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,21 @@ const startServer = async () => {
   try {
     const app = express();
 
-    const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
+    const allowedOrigins =
+      process.env.NODE_ENV === "production"
+        ? [process.env.PRODUCTION_FRONTEND_URL]
+        : [process.env.DEVELOPMENT_FRONTEND_URL];
+
+    if (process.env.NODE_ENV !== "production") {
+      console.log("🌐 Allowed Origins:", allowedOrigins);
+    }
 
     app.use((req, res, next) => {
-      console.log("Incoming Origin:", req.headers.origin);
-      console.log("Request URL:", req.url);
-      console.log("Request Method:", req.method);
+      if (process.env.NODE_ENV !== "production") {
+        console.log("🚀 Incoming Origin:", req.headers.origin);
+        console.log("📄 Request URL:", req.url);
+        console.log("⚙️ Request Method:", req.method);
+      }
       next();
     });
 
@@ -24,7 +33,7 @@ const startServer = async () => {
           if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
           } else {
-            console.error(`Blocked by CORS: ${origin}`);
+            console.error(`❌ Blocked by CORS: ${origin}`);
             callback(new Error("Not allowed by CORS"));
           }
         },
@@ -41,17 +50,30 @@ const startServer = async () => {
     app.use("/api", nftRoutes);
 
     app.get("/api/", (req, res) => {
-      res.send("CORS is working properly. Welcome to NFT Marketplace!");
+      res.send("🎉 CORS is working properly. Welcome to NFT Marketplace!");
     });
 
     app.use(errorHandler);
 
     const PORT = process.env.SERVER_PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+
+    if (process.env.NODE_ENV !== "production") {
+      app.listen(PORT, () => {
+        console.log(`🌟 Server running on http://localhost:${PORT}`);
+      });
+    } else {
+      app.listen(PORT, () => {
+        console.log(
+          `🚀 Server running in ${process.env.NODE_ENV} environment!`
+        );
+      });
+    }
   } catch (error) {
-    console.error("Error initializing the server:", error.message, error.stack);
+    console.error(
+      "❗ Error initializing the server:",
+      error.message,
+      error.stack
+    );
     process.exit(1);
   }
 };
