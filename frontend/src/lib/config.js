4cfe1fc1d290a4c37ae259b4204eb9a21c0faf9b
Aradhya Jain <ajain@qodeleaf.com>
2024-12-11 06:02:08 +0530

    Feat: Get the backend url for respective environment variable set for NEXT_PUBLIC_ENV

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -2,10 +2,10 @@ const ENV = process.env.NEXT_PUBLIC_ENV || "development";
 
 const CONFIG = {
   development: {
-    backendUrl: process.env.NEXT_PUBLIC_DEVELOPMENT_BACKEND_URL,
+    backendUrl: `${process.env.NEXT_PUBLIC_DEVELOPMENT_BACKEND_URL}/api`,
   },
   production: {
-    backendUrl: process.env.NEXT_PUBLIC_PRODUCTION_BACKEND_URL,
+    backendUrl: `${process.env.NEXT_PUBLIC_PRODUCTION_BACKEND_URL}/api`,
   },
 };
 
