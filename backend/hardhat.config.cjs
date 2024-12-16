01b86f86ca9ffe6ee10947ca02ed00c77538b16f
Aradhya Jain <ajain@qodeleaf.com>
2024-12-16 18:06:52 +0530

    Update: Renamed extension of hardhat.config to cjs from js as we have defined our package as ES Module

--- backend/hardhat.config.cjs
+++ backend/hardhat.config.cjs
@@ -1,11 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 const { INFURA_SEPOLIA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: {
-    version: "0.8.20",
+    version: '0.8.20',
     settings: {
       optimizer: {
         enabled: true,
@@ -15,11 +15,11 @@ module.exports = {
   },
   networks: {
     sepolia: {
-      url: INFURA_SEPOLIA_RPC_URL || "",
+      url: INFURA_SEPOLIA_RPC_URL || '',
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY || "",
+    apiKey: ETHERSCAN_API_KEY || '',
   },
 };
